0fc34d542e54e1ce4121f572ccf0d393bfefbc2a
Shivam Kumar Nayak <snayak@datopic.com>
2024-10-16 18:03:30 +0530

    Add word file convertor

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');  // Enable CORS for cross-origin requests
 const excelToPdfRouter = require('./routes/excelToPdfRouter');  // Import the router
+const wordToPdfRouter = require('./routes/wordToPdfRouter');
 
 const app = express();
 app.use(cors());  // Allow cross-origin requests from React
 
 // Use the excelToPdfRouter for the "/excel-to-pdf" route
 app.use('/excel-to-pdf', excelToPdfRouter);
+// Use the wordToPdfRouter for the "/word-to-pdf" route
+app.use('/word-to-pdf', wordToPdfRouter);
 
 // Start the server
 app.listen(3001, () => {
