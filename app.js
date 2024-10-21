83107ba624edfc5aa94daf91ec8d1fdd2ff062b1
Shivam Kumar Nayak <snayak@datopic.com>
2024-10-21 18:10:12 +0530

    Adding environment variable file and .gitIgnore file

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');  // Enable CORS for cross-origin requests
 const excelToPdfRouter = require('./routes/excelToPdfRouter');  // Import the router
 const wordToPdfRouter = require('./routes/wordToPdfRouter');
+const dotenv = require('dotenv');
+
+
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 app.use(cors());  // Allow cross-origin requests from React
@@ -12,6 +16,6 @@ app.use('/excel-to-pdf', excelToPdfRouter);
 app.use('/word-to-pdf', wordToPdfRouter);
 
 // Start the server
-app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001');
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
